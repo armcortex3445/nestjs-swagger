4137f774651da3c2355e10bf1dee8307920401ab
armcortex3445 <wlghks5966@naver.com>
2025-07-23 19:02:57 +0900

    feat(app): import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { HumansModule } from './humans/humans.module';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, DogsModule, HumansModule],
 })
 export class AppModule {}
