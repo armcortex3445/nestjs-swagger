7a77dac29d58970a6bb9a2f833f412bd1d93dbdc
armcortex3445 <wlghks5966@naver.com>
2025-07-24 15:18:44 +0900

    feat(main): add global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const options = new DocumentBuilder()
-    .setTitle('swagger example')
-    .setDescription('main server API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document,{
-    jsonDocumentUrl : 'api-json',
-  });
+  configPipe(app);
+
+  attachSwagger(app);
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+
+function attachSwagger(app : INestApplication<any>){
+
+  const options = new DocumentBuilder()
+  .setTitle('swagger example')
+  .setDescription('main server API description')
+  .setVersion('1.0')
+  .addTag('cats')
+  .addBearerAuth()
+  .build();
+const document = SwaggerModule.createDocument(app, options);
+SwaggerModule.setup('api', app, document,{
+  jsonDocumentUrl : 'api-json',
+});
+}
+
+function configPipe(app : INestApplication<any>){
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform : true,
+  }));
+}
