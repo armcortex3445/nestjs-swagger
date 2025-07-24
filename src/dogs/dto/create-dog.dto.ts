2816855f6e6358de5b586a6684fa85a20181ac35
armcortex3445 <wlghks5966@naver.com>
2025-07-24 15:19:45 +0900

    feat(dog): validate DTO

--- src/dogs/dto/create-dog.dto.ts
+++ src/dogs/dto/create-dog.dto.ts
@@ -1,10 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { DOG_BREED, DogBreedValue } from "../entities/dog.entity";
+import { IsIn, IsNumber, IsString } from "class-validator";
 
 export class CreateDogDto {
+    @IsString()
     name! : string;
+
+    @IsNumber()
     age! : number;
 
+    @IsIn([...Object.values(DOG_BREED)])
     @ApiProperty({enum : [...Object.values(DOG_BREED)] })
     breed! : DogBreedValue;
 }
